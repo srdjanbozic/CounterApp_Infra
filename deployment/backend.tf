fdd59dc653de48ba2943d1058df757f5032a8b66
Srdjan Bozic <srdjanbozic@protonmail.com>
2025-10-08 13:35:05 +0200

    Complete multi-environment setup: DEV, QA, PROD with health checks

--- deployment/backend.tf
+++ deployment/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "kubernetes" {
-    secret_suffix    = "state"
-    config_path      = "~/.kube/config"
-    config_context   = "minikube"
-    namespace        = "terraform-state"
+    secret_suffix  = "state"
+    config_path    = "~/.kube/config"
+    config_context = "minikube"
+    namespace      = "terraform-state"
   }
 }
